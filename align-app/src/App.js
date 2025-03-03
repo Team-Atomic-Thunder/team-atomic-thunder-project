c5a8f3d6b517627063b23a9d53c294cc468432d5
Meghann Manson <meghann.manson@maine.edu>
2025-03-03 13:23:31 -0500

    basic App.js outline

--- align-app/src/App.js
+++ align-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import HomePage from './pages/HomePage';
+import Dashboard from './pages/Dashboard';
+import UploadPage from './pages/UploadPage';
+import CalendarPage from './pages/CalendarPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
