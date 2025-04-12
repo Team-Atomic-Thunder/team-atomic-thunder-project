ec8be5be35730933fa565e49076045d3acfb64fa
meghann <meghann.manson@maine.edu>
2025-04-12 15:42:22 -0400

    fixed failing test to pass

--- align-app/src/components/TestComponent.failing.test.js
+++ align-app/src/components/TestComponent.failing.test.js
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 describe('Intentional Failing Test', () => {
   test('this test will fail to verify workflow', () => {
     // This assertion will fail, triggering the workflow
-    expect(true).toBe(false);
+    expect(true).toBe(true);
   });
   
   // You can also include a test that passes to see both results
